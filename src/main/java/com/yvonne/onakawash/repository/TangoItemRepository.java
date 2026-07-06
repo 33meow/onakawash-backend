5c75964a48d83a8b87d63b5b8b2d9a7b9e4e73c0
Buttercup <mayiyuanchenze@gmail.com>
2026-07-06 10:19:17 +0800

    BE-2: Add TangoItem repository（添加词汇条目仓库）

---
+++ src/main/java/com/yvonne/onakawash/repository/TangoItemRepository.java
@@ -0,0 +1,7 @@
+package com.yvonne.onakawash.repository;
+
+import com.yvonne.onakawash.entity.TangoItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TangoItemRepository extends JpaRepository<TangoItemEntity, String> {
+}
